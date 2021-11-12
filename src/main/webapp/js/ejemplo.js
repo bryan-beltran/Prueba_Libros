a9d399614655666108798a890312f39187ba48d1
raton <raton@192.168.0.14>
2021-11-12 16:15:33 -0500

    Ejemplo JS

---
+++ src/main/webapp/js/ejemplo.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {//Trabajando Jquery
+
+	alert("Hola Mundo")
+
+	$('.sumar').on('click',function() {
+		sumar()
+	})
+	
+	function sumar(){	
+		var num1 = parseInt(document.getElementById("n1").value)
+		var num1 = parseInt(document.getElementById("n2").value)
+		var suma = num1 + num2
+
+		alert("La suma es: " +suma)
+		console.log("La suma es: " +suma)
+		var res = document.getElementById("resultado")
+		res.innerHTML = "La suma es: " +suma
+	}
+	
+})
